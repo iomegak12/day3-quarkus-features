d9eb442ccc2fadd5cfedc3f6d586beb94ed3cbd8
iomegak12 <jd.ramkumar@gmail.com>
2025-07-30 12:35:47 +0530

    day 3 - fault tolerance features

--- src/main/java/com/learning/developers/SwapiService.java
+++ src/main/java/com/learning/developers/SwapiService.java
@@ -1,17 +1,28 @@
 package com.learning.developers;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.*;
+import org.eclipse.microprofile.faulttolerance.*;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.core.MediaType;
 
 @RegisterRestClient
 public interface SwapiService {
-
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     @Path("/films/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(value = 2000L)
+    @Fallback(SwapiFallback.class)
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = .5, delay = 5000L, successThreshold = 2)
     public Swapi getFilmById(@PathParam("id") String id);
+
+    public static class SwapiFallback implements FallbackHandler<Swapi> {
+
+        private static final Swapi EMPTY_SWAPI = new Swapi("", 0, "", "", "");
+
+        @Override
+        public Swapi handle(ExecutionContext context) {
+            return EMPTY_SWAPI;
+        }
+
+    }
 }
